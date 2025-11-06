2577e8e8ed1a3ce88fae8360e36adddf4d33fd05
n0o0b090lv <ernestspursw@gmail.com>
2025-11-06 22:06:47 +0200

    Added pointer aka the laser as text str

--- Scripts/gun_visual.gd
+++ Scripts/gun_visual.gd
@@ -1,20 +1,52 @@
 extends Node
 
+@export_group("assignments")
 @export var LOOK_AT: Node3D
 @export var GUN: Node3D
 @export var POINTER: Label3D
+@export var POINTER_LEFT: Label3D
 @export var POINTER_GOAL: MeshInstance3D
 @export var POINTER_GROUND: MeshInstance3D
 @export var POINTER_GROUND_NORM: MeshInstance3D
+@export_group("others")
+@export var smoothing: float = 1
+var prev_location = Vector3.FORWARD
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	POINTER.scroll_dir = -10
+	POINTER_LEFT.scroll_dir = -10
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	GUN.look_at(LOOK_AT.global_position)
-	var dist = GUN.global_position.distance_to(LOOK_AT.global_position)
+	#Smooths cursor
+	prev_location += (LOOK_AT.global_position - prev_location) * delta * smoothing
+
+	#Detects if there aren't no obstacles infront
+	var temp_raycast = castRay()
+	var look_position = prev_location
+	if temp_raycast:
+		look_position = temp_raycast["position"]
+
+	var dist = GUN.global_position.distance_to(look_position)
+
+	GUN.look_at(look_position)
+
+	POINTER_GOAL.global_position = look_position
+
 	POINTER.distance = dist
 	POINTER.renderText()
-	POINTER.position.z = -0.8 + dist / -2
+	POINTER.position.z = -0.4 + dist / -2
+	POINTER_LEFT.distance = dist
+	POINTER_LEFT.renderText()
+	POINTER_LEFT.position.z = -0.4 + dist / -2
+
+func castRay():
+	var ray_point_start = GUN.global_position
+	var ray_point_end = prev_location
+
+	var space_state = GUN.get_world_3d().direct_space_state
+
+	var params = PhysicsRayQueryParameters3D.create(ray_point_start, ray_point_end, 2)
+
+	return space_state.intersect_ray(params)
--- Scripts/label_sizer.gd
+++ Scripts/label_sizer.gd
@@ -16,20 +16,27 @@ var TEXT_SIZE = [
 	0.325
 ]
 var distance = 4
+var scroll_dir = -10
 @export var text_state := Text_States.IDLE
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	renderText()
 
-
+var scroll = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	scroll -= delta * scroll_dir
 
 func setState(p_state):
 	text_state = p_state
 
 func renderText():
 	text = ""
-	for i in range(floor(distance / (TEXT_SIZE[text_state] * 4))):
-		text += SHOT_TEXTS[text_state]
+	var temp_range = floori(distance / (TEXT_SIZE[text_state] * 4)) - 1
+	for i in range(temp_range):
+		if i == 0:
+			text += SHOT_TEXTS[text_state].substr(floori(scroll) % (SHOT_TEXTS[text_state].length()))
+		elif i == temp_range - 1:
+			text += SHOT_TEXTS[text_state].substr(0, floori(scroll) % (SHOT_TEXTS[text_state].length()))
+		else:
+			text += SHOT_TEXTS[text_state]
