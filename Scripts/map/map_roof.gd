9c3f7b7f70baba99798609d2f8bbf7a78f58a7b8
officialErnests <ernestspursw@gmail.com>
2025-11-27 20:05:09 +0200

    v0.1.7.1.1 broke shit sob

--- Scripts/map/map_roof.gd
+++ Scripts/map/map_roof.gd
@@ -1,18 +1,20 @@
 class_name building_roof extends Node3D
 
 @export var player_closenes = -1
+@export var visible_distance = 1
 @export var light_distance = 1
 @export var neibour_roofs: Array[building_roof]
 
 @onready var visual :MeshInstance3D= $Visual
 @onready var trigger :Area3D = $Trigger
+@onready var light: Light3D = $Visual/SpotLight3D
 var update_step = 0
 var seek_step = 0
 
 func _ready() -> void:
 	trigger.body_entered.connect(roomEntered)
 	trigger.body_exited.connect(roomExited)
-	
+
 func roomEntered(body) -> void:
 	if body.is_in_group("Player"):
 		player_closenes = 0
@@ -27,20 +29,42 @@ func expandZero(p_value, p_update_step) -> void:
 	if update_step == p_update_step:
 		if player_closenes >= p_value:
 			player_closenes = p_value
-			print("AHHHHHHHHHHHH")
+			updateRoof()
 			for roof in neibour_roofs:
 				roof.expandZero(p_value + 1, p_update_step)
 		return
-		
+	
 	update_step = p_update_step
+	if player_closenes == 0:
+		updateRoof()
+		for roof in neibour_roofs:
+			roof.expandZero(1, p_update_step)
+		return
 	player_closenes = p_value
+	updateRoof()
 	for roof in neibour_roofs:
 		roof.expandZero(p_value + 1, p_update_step)
 
-func seekZero(p_seek_step) -> void:
+func seekZero(p_seek_step) -> int:
 	if seek_step == p_seek_step:
-		return
+		return -1
+	if player_closenes == 0:
+		return true
 	player_closenes = -1
 	seek_step = p_seek_step
+	updateRoof()
 	for roof in neibour_roofs:
-		roof.seekZero(p_seek_step)
+		variable_return = roof.seekZero(p_seek_step)
+		if variable_return != -1:
+			return variable_return + 1
+	return -1
+
+func updateRoof():
+	debug()
+	if player_closenes > light_distance or player_closenes == -1:
+		light.visible = false
+	else:
+		light.visible = true
+
+func debug():
+	$Label3D.text = str(player_closenes)
